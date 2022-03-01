b3125d8f81cd9ac7036daf3c3e218c5493cab0cc
Lukas Schmidt <luki.schmidt@gmx.at>
2022-03-01 12:31:24 +0100

    Animation for Hashashin added

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,5 +3,18 @@ extends KinematicBody2D
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("right")
+	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("right")
+	elif Input.is_action_pressed("ui_up"):
+		$AnimatedSprite.play("jump")
+	elif Input.is_action_pressed("ui_down"):
+		$AnimatedSprite.play("down")
+	elif Input.is_action_pressed("attack_1"):
+		$AnimatedSprite.play("attack_basic")
+	else:
+		$AnimatedSprite.play("idle")
+		
 		
